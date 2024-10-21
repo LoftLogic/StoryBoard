05258ab25f2d5e7bafa90e7815ec0209d71984da
LoftLogic <evanlie.737@gmail.com>
2024-10-20 21:37:47 -0400

    First Creation

---
+++ kilo-ed/target/debug/deps/kilo_ed.d
@@ -0,0 +1,5 @@
+C:\Users\evanl\StoryBoard\kilo-ed\target\debug\deps\kilo_ed.exe: src/main.rs
+
+C:\Users\evanl\StoryBoard\kilo-ed\target\debug\deps\kilo_ed.d: src/main.rs
+
+src/main.rs:
---
+++ kilo-ed/target/debug/kilo-ed.d
@@ -0,0 +1,1 @@
+C:\Users\evanl\StoryBoard\kilo-ed\target\debug\kilo-ed.exe: C:\Users\evanl\StoryBoard\kilo-ed\src\main.rs
